4fb7248dc84748d75df537a77072eeba22ece254
Arsen Melikyan <bugron@mail.ru>
2021-02-20 21:39:20 +0400

    First working prototype of terminology search extension

--- README.md
+++ README.md
@@ -1,23 +1,18 @@
-# Google Search
-A Simple Extension for VSCode to search easily using Google
-
-### If you use [bing](https://marketplace.visualstudio.com/items?itemName=kameshkotwani.bing-search), I have an extension for that too!
-# What it does:
-Well, if you are stuck in some confusion about what this or that line or this function does, then you can simply select the function or line you want and vola! you'll get instant search results for that query in your default web-browser without needing to type or copy the whole thing up!
+# Microsoft Terminology Search
+A Simple Extension for VSCode to search easily using Microsoft Terminology Search
 
 ## How it works!
-Simply select the word or function you want to search, then right click and click on `Google this` in drop-down menu.
+Simply select the word or function you want to search, then right click and click on `Search with Microsoft Terminology` in drop-down menu.
 
 # See it in Action
-#### when you select the text and right click you'll see `Google this` in your drop-down menu
+When you select the text and right click you'll see `Search with Microsoft Terminology` in your drop-down menu
 
-![drop-down-menu](https://github.com/kameshkotwani/google-search/blob/master/resources/drop-down-menu.png?raw=true)
+![drop-down-menu](/resources/drop-down-menu.png?raw=true)
 
-## How it opens in browser
+## How it opens in VSCode
 
-![browser-query-results](https://github.com/kameshkotwani/google-search/blob/master/resources/search-results.png?raw=true)
+![browser-query-results](/resources/search-results.png?raw=true)
 
+### If you encounter any issues, please let me know [here](https://www.github.com/bugron/microsoft-terminology-search).
 
-### If you encounter any issues, please let me know [here](https://github.com/kameshkotwani/google-search/issues)
-
-#### This extension was made possible by [Ciprian Burca](https://github.com/burcadoruciprian)
+#### This extension is based on the source code of [google-search](https://github.com/kameshkotwani/google-search/).
--- extension.js
+++ extension.js
@@ -1,15 +1,40 @@
 //Strings
-const CFG_SECTION = "googleSearch";
-const CFG_QUERY = "QueryTemplate";
-const CMD_ID = "extension.googleSearch";
+const CFG_SECTION = 'terminologySearch';
+const CFG_FROM = 'TranslateFrom';
+const CFG_INTO = 'TranslateInto';
+const CFG_OPERATOR = 'SearchOperator';
+const CFG_CONFIDENCE = 'ConfidenceLevel';
+const CMD_ID = 'extension.terminologySearch';
+const xml = `
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns="http://api.terminology.microsoft.com/terminology">
+    <soapenv:Header/>
+    <soapenv:Body>
+        <GetTranslations>
+            <text>{{selectedText}}</text>
+            <from>{{languageFrom}}</from>
+            <to>{{languageInto}}</to>
+            <searchOperator>{{searchOperator}}</searchOperator>
+            <sources>
+                <TranslationSource>Terms</TranslationSource>
+                <TranslationSource>UiStrings</TranslationSource>
+            </sources>
+            <unique>false</unique>
+            <maxTranslations>20</maxTranslations>
+            <includeDefinitions>true</includeDefinitions>
+        </GetTranslations>
+    </soapenv:Body>
+</soapenv:Envelope>`;
+const soapURL = 'https://api.terminology.microsoft.com/Terminology.svc';
 
-const vscode = require("vscode");
+const vscode = require('vscode');
+const xml2js = require('xml2js');
+const axios = require('axios');
 
 //Activating Function
 function activate(context) {
   const disposable = vscode.commands.registerTextEditorCommand(
     CMD_ID,
-    webSearch
+    terminologySearch
   );
   context.subscriptions.push(disposable);
 }
@@ -20,27 +45,86 @@ function deactivate() {}
 exports.deactivate = deactivate;
 
 //Function to launch the Search URL in default browser
-function webSearch() {
+function terminologySearch() {
   const selectedText = getSelectedText();
   if (!selectedText) {
     return;
   }
-  const uriText = encodeURI(selectedText);
-  const googleSearchCfg = vscode.workspace.getConfiguration(CFG_SECTION);
-  const queryTemplate = googleSearchCfg.get(CFG_QUERY);
-  const query = queryTemplate.replace("%SELECTION%", uriText);
-  vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(query));
+  const terminologySearchCfg = vscode.workspace.getConfiguration(CFG_SECTION);
+  const languageFrom = terminologySearchCfg.get(CFG_FROM);
+  const languageInto = terminologySearchCfg.get(CFG_INTO);
+  const searchOperator = terminologySearchCfg.get(CFG_OPERATOR);
+  const confidenceLevel = terminologySearchCfg.get(CFG_CONFIDENCE);
+  const requestXML = xml
+    .replace('{{selectedText}}', selectedText)
+    .replace('{{languageFrom}}', languageFrom || 'en-US')
+    .replace('{{languageInto}}', languageInto || 'ru-RU')
+    .replace('{{searchOperator}}', searchOperator || 'Exact');
+
+  console.log('requestXML', requestXML);
+  // @ts-ignore
+  axios.post(soapURL, requestXML, {
+    headers: {
+      'Content-Type': 'text/xml;charset=UTF-8',
+      'SOAPAction': 'http://api.terminology.microsoft.com/terminology/Terminology/GetTranslations',
+    }
+  })
+    .then(({ data: xmlData }) => {
+      console.log('xmlData', xmlData);
+      return xml2js.parseStringPromise(xmlData, { explicitArray: false, ignoreAttrs: true });
+    })
+    .then(data => {
+      const allMatches = data['s:Envelope']['s:Body'].GetTranslationsResponse.GetTranslationsResult.Match;
+      console.log('Parsed XML', allMatches);
+      const translations = allMatches
+        .filter(match => parseInt(match.ConfidenceLevel, 10) >= confidenceLevel)
+        .map(match => ({ text: match.OriginalText, translation: match.Translations.Translation.TranslatedText }));
+      console.log('translations', translations);
+      const panel = vscode.window.createWebviewPanel(
+        'terminologySearch', // Identifies the type of the webview. Used internally
+        'Terminology Search Results', // Title of the panel displayed to the user
+        vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+      );
+
+      // And set its HTML content
+      panel.webview.html = getWebviewContent(translations);
+    })
+    .catch(err => {
+      console.log(err);
+      vscode.window.showErrorMessage(
+        'An error occured! ' + err.message
+      );
+    })
+  // vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(query));
+}
+
+
+function getWebviewContent(translations) {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Terminology Search Results</title>
+</head>
+<body>
+  <ol>
+    ${translations.map(t => `<li>${t.text}: ${t.translation}</li>`).join('\n')}
+  </ol>
+</body>
+</html>`;
 }
 
 //getSelectedText creates a URL for search based on the selection
 function getSelectedText() {
   const documentText = vscode.window.activeTextEditor.document.getText();
   if (!documentText) {
-    return "";
+    return '';
   }
   const activeSelection = vscode.window.activeTextEditor.selection;
   if (activeSelection.isEmpty) {
-    return "";
+    return '';
   }
   const selStartoffset = vscode.window.activeTextEditor.document.offsetAt(
     activeSelection.start
@@ -50,5 +134,5 @@ function getSelectedText() {
   );
 
   let selectedText = documentText.slice(selStartoffset, selEndOffset).trim();
-  return selectedText.replace(/\s\s+/g, " ");
+  return selectedText.replace(/\s\s+/g, ' ');
 }
