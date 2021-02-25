841bbcafccf7bc3e9fda7159d01e106ef0d724ce
Arsen Melikyan <bugron@mail.ru>
2021-02-25 16:16:32 +0400

    Use search text as the title of VSCode tab for better navigation

--- extension.js
+++ extension.js
@@ -88,7 +88,7 @@ function terminologySearch() {
 
       const panel = vscode.window.createWebviewPanel(
         'terminologySearch', // Identifies the type of the webview. Used internally
-        'Terminology Search Results', // Title of the panel displayed to the user
+        selectedText, // Title of the panel displayed to the user
         vscode.ViewColumn.One, // Editor column to show the new webview panel in.
         {} // Webview options. More on these later.
       );
